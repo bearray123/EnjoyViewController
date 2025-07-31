c7cd6c89ece420be103d1cd795d91122929d6e84
rayxiong <rayxiong@didiglobal.com>
2025-07-31 19:52:41 +0800

    首次引入SDWebImage测试第一张pic

--- EnjoyViewController/home/MainViewController.m
+++ EnjoyViewController/home/MainViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "MainViewController.h"
+#import <SDWebImage/SDWebImage.h>
 
 @interface MainViewController ()
 
@@ -17,6 +18,28 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     NSLog(@"MainViewController:: viewDidLoad()");
+    
+    // 1. 创建一个 UIImageView
+       UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 100, 300, 200)];
+       imageView.contentMode = UIViewContentModeScaleAspectFill;
+       imageView.clipsToBounds = YES;
+       imageView.backgroundColor = [UIColor lightGrayColor]; // 加载前的灰底背景
+       [self.view addSubview:imageView];
+
+       // 2. 使用 SDWebImage 加载网络图片
+       NSURL *imageURL = [NSURL URLWithString:@"https://image.xunyou.com/image/201907/1563850242247.jpg"];
+       [imageView sd_setImageWithURL:imageURL
+                    placeholderImage:[UIImage imageNamed:@"placeholder"]
+                             options:SDWebImageRetryFailed | SDWebImageLowPriority
+                           completed:^(UIImage * _Nullable image, NSError * _Nullable error,
+                                      SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+           if (error) {
+               NSLog(@"图片加载失败: %@", error);
+           } else {
+               NSLog(@"图片加载成功，来自缓存类型: %ld", (long)cacheType);
+           }
+       }];
+    
 }
 
 
